dbf1e88c8a4ef990b062aeb328c0138b60c0a87c
benjamin.tardieu <benjamin.tardieu@gmail.com>
2010-12-26 13:46:29 +0000

    Suppression de la classe "Main" du package "main", et mise à jour des classes dans le package "client".

---
+++ Projet-RMI/src/client/AgenceClient.java
@@ -0,0 +1,12 @@
+package client;
+
+import ihm.Fenetre;
+import ihm.PanelAgence;
+
+public class AgenceClient
+{
+	public static void main(String args[]) 
+	{        
+		Fenetre fen2 = new Fenetre(new PanelAgence(),"Agence");
+	}
+}
